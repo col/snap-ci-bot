41266063ddefdfea4025fef6ea74acbb03aa496a
Col Harris <col.w.harris@gmail.com>
2016-05-01 23:20:22 +0800

    Removed some debugging

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var token = process.env['TELEGRAM_BOT_TOKEN'];
 var chatId = process.env['TELEGRAM_CHAT_ID'];
 
 exports.handler = function(event, context, callback) {
-  console.log("event = ", event);
-  console.log("context = ", context);
   var bot = new TelegramBot(token, {polling: false});
   var message = event.project_name + ' - ' + event.stage_name + ' #' + event.counter + ': ' + event.build_result;
   bot.sendMessage(chatId, message);
